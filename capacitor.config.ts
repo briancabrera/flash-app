9dc8acf22d2bd6fe65e6fe316a67f54b04d55870
Brian Cabrera <briancabrer4@gmail.com>
2024-11-11 03:03:08 -0300

    iOS capacitor

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,12 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.brian.flashapp',
   appName: 'Flash',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    Camera: {
+      cameraPermissionText: "We need your permission to use your camera to scan QR codes"
+    }
+  }
 };
 
 export default config;
