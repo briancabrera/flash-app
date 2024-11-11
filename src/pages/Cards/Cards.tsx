1f736b22a5376d06c919aa325a135405f42bdf3f
Brian Cabrera <briancabrer4@gmail.com>
2024-11-11 18:02:33 -0300

    cards - v2 final

--- src/pages/Cards/Cards.tsx
+++ src/pages/Cards/Cards.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { useHistory } from 'react-router-dom';
 import { 
   IonContent, 
@@ -16,7 +16,8 @@ import {
   IonCardContent,
   IonFooter
 } from '@ionic/react';
-import { close, chevronForward, addOutline } from 'ionicons/icons';
+import { close, chevronForward } from 'ionicons/icons';
+import FloatingLightningBolts from '../../components/ui/FloatingLightningBolts/FloatingLightningBolts';
 import styles from './Cards.module.scss';
 
 interface Card {
@@ -42,26 +43,42 @@ const Cards: React.FC = () => {
       name: 'Crédito BBVA',
       type: 'mastercard',
       lastDigits: '4321'
+    },
+    {
+      id: '3',
+      name: 'Débito Scotiabank',
+      type: 'visa',
+      lastDigits: '5678'
+    },
+    {
+      id: '4',
+      name: 'Crédito Santander',
+      type: 'mastercard',
+      lastDigits: '9012'
     }
   ];
 
+  const memoizedFloatingLightningBolts = useMemo(() => <FloatingLightningBolts />, []);
+
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar color="dark">
+      <IonHeader className={styles.header}>
+        <IonToolbar>
           <IonTitle>Flash</IonTitle>
           <IonButtons slot="end">
-            <IonButton onClick={() => history.push('/account')}>
-              <IonIcon icon={close} />
+            <IonButton className={styles.cancelButton} onClick={() => history.push('/account')}>
+              <IonIcon className={styles.closeIcon} icon={close} />
             </IonButton>
           </IonButtons>
         </IonToolbar>
       </IonHeader>
 
       <IonContent fullscreen className={styles.content}>
+        {memoizedFloatingLightningBolts}
+        
         <h1 className={styles.title}>Mis tarjetas</h1>
 
-        <IonList>
+        <IonList className={styles.cardsList}>
           {cards.map(card => (
             <IonCard key={card.id} className={styles.cardItem} onClick={() => history.push(`/card/${card.id}`)}>
               <IonCardContent>
@@ -79,14 +96,13 @@ const Cards: React.FC = () => {
         </IonList>
       </IonContent>
 
-      <IonFooter>
+      <IonFooter className={styles.footer}>
         <IonToolbar>
           <IonButton 
             expand="block" 
             onClick={() => history.push('/card/new')}
             className={styles.addButton}
           >
-            <IonIcon icon={addOutline} slot="start" />
             Agregar tarjeta
           </IonButton>
         </IonToolbar>
